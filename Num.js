d30b781d943fcedaa9914d6c568462767a4d5939
charlotte-2222 <ayy.charlotte@gmail.com>
2022-06-14 16:03:49 -0400

    Update:
    - added a word count box. now you can just type a for an hour and see what happens

--- Num.js
+++ Num.js
@@ -15,3 +15,24 @@ function decrementBtn(){
     console.log(value)
     document.getElementById('decrementText').innerHTML=value;
 }
+
+function countChars(obj){
+    document.getElementById("charNum").innerHTML = obj.value.length;
+
+}
+
+$(function() {
+    $('#myTextArea').on('input keyup paste', function() {
+        var $el = $(this),
+            offset = $el.innerHeight() - $el.height();
+
+        if ($el.innerHeight() < this.scrollHeight) {
+            // Grow the field if scroll height is smaller
+            $el.height(this.scrollHeight - offset);
+        } else {
+            // Shrink the field and then re-set it to the scroll height in case it needs to shrink
+            $el.height(1);
+            $el.height(this.scrollHeight - offset);
+        }
+    });
+});
